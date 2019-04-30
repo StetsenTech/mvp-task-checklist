33acbd06494d82a371cec82de38601bd42b39f0c
Brian Rose <stetsentech@gmail.com>
2019-04-30 14:53:50 -0400

    Basic routing layout
    - Lays out routing for `/projects`, `/tasks`, `/users`

--- app.py
+++ app.py
@@ -1,13 +1,110 @@
-from flask import Flask, jsonify
-from requests import Response
+from flask import Flask, jsonify, make_response
+from flask_login import LoginManager, login_required
 
 app = Flask(__name__)
 
-@app.route("/")
-def checklist():
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@app.route('/')
+def status():
     message = {
-        "message": "success"
+        'message': 'success'
     }
 
-    return jsonify(message), 200
+    return make_response(jsonify(message), 200)
 
+@app.route('/users')
+def get_users():
+    """Gets a list of all users in the system
+
+    Return:
+        Response containing an array of all user dictionareis
+    """
+    pass
+
+@app.route('/users/', methods=["POST"])
+def create_user():
+    """Creates a new user
+    """
+    pass
+
+@app.route('/users/<user_id>', methods=["GET"])
+def get_user(user_id:int):
+    """Returns a list of all users
+    
+    Arguments:
+        user_id {int} -- ID of user to get information for
+    
+    Return:
+        Resposne containing a users information
+    """
+    pass
+
+@app.route('/users/<user_id>/tasks', methods=["GET"])
+def get_user(user_id:int):
+    """Returns a list of all tasks for users
+    
+    Arguments:
+        user_id {int} -- ID of user to get information for
+    
+    Return:
+        Resposne containing a list of users tasks
+    """
+    pass
+
+@app.route('/projects/', methods=["GET"])
+def get_projects():
+    """Gets a list of all projects
+
+    Return:
+        List of all projects available
+    """
+    pass
+
+
+@app.route('/projects/', methods=["POST"])
+def create_project():
+    """Creates a new project
+    """
+    pass
+
+@app.route('/projects/<project_id>')
+def get_project(project_id:int):
+    """Gets a specific project
+    
+    Arguments:
+        project_id {int} -- ID of project to get information for
+    """
+    pass
+
+@app.route('/projects/<project_id>/tasks')
+def get_project_tasks(project_id:int):
+    """Gets all tasks for a specific project
+    
+    Arguments:
+        project_id {int} -- ID of project to get tasks for
+    """
+    pass
+
+@app.route("/tasks/")
+@login_required
+def get_tasks():
+    """Gets tasks for current user
+    """
+    pass
+
+@app.route("/tasks/", methods=["POST"])
+def create_task():
+    """Creates a new task
+    """
+    pass
+
+@app.route('/tasks/<task_id>')
+def get_task(task_id:int):
+    """Gets a specific task
+    
+    Arguments:
+        task_id {int} -- ID of task to get information about
+    """
+    pass
